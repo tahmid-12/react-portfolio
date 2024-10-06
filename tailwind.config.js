45587e5521be3e6f3ff8552960e041703cfcf3bc
tahmid-12 <mdtahmidchowdhury12@gmail.com>
2024-10-07 00:44:59 +0600

    Tailwind configured

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export const content = [
+  "./src/**/*.{js,jsx,ts,tsx}", // Includes all JavaScript and TypeScript files
+  "./src/components/**/*.{js,jsx,ts,tsx}", // For components you will add
+];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
