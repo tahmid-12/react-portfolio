76859ce69117dd9b14db37f1093397135c973550
tahmid-12 <mdtahmidchowdhury12@gmail.com>
2024-10-14 00:25:47 +0600

    Contact Section added

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import About from './Components/About'
 import Technologies from './Components/Technologies'
 import Experience from './Components/Experience'
 import Projects from './Components/Projects'
+import Contact from './Components/Contact'
 
 function App() {
 
@@ -23,6 +24,7 @@ function App() {
         <Technologies />
         <Experience />
         <Projects />
+        <Contact />
       </div>
     </div>
   )
